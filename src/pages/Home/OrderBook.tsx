bba5de1aae302da015ac7f1609c1629d6a4117ce
andoan <secret.andoan@gmail.com>
2024-02-08 00:11:00 +0700

    add refetchInterval for order book

--- src/pages/Home/OrderBook.tsx
+++ src/pages/Home/OrderBook.tsx
@@ -6,10 +6,12 @@ import { Box, Flex, Type } from 'theme/base'
 import { formatNumber, formatRelativeShortDate } from 'utils/helpers/format'
 
 export default function OrderBook() {
-  const { data } = useQuery('market-orders', () => getOrders())
+  const { data } = useQuery('market-orders', () => getOrders(), {
+    refetchInterval: 5000,
+  })
   return (
     <Box width="100%" p={3}>
-      <Flex sx={{ '& > *': { flexShrink: 0 } }} mb={2} color="neutral5">
+      <Flex sx={{ '& > *': { flexShrink: 0 } }} mb={12} color="neutral5">
         <Type.Small width="30%">
           <Trans>Time</Trans>
         </Type.Small>
